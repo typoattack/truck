e503e88d8499324bc0b32664aa704da4794386ae
sparK1N9 <spacecqcenhancer@gmail.com>
2019-09-11 03:35:35 -0400

    updated coin behavior -coins now move from -2 to 2 -each road has 30% chance of being able to spawn a coin -coin script now seperate from truck

---
+++ Scripts/CoinController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.right * speed;
+        transform.Rotate(new Vector3(90, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        if (rb.transform.position.x > 2)
+        {
+            rb.velocity = new Vector3(speed * -1, 0f, 0f);
+        }
+        else if (rb.transform.position.x < -2) rb.velocity = new Vector3(speed, 0f, 0f);
+
+        if (transform.position.z <= -2.0f) Destroy(gameObject);
+    }
+}
--- Scripts/TruckController.cs
+++ Scripts/TruckController.cs
@@ -14,27 +14,17 @@ public class TruckController : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
 		direction = transform.right;
         rb.velocity = transform.right * speed;
-        if (gameObject.tag == "Coin") transform.Rotate(new Vector3(90, 0, 0));
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (gameObject.tag == "Coin")
-        {
-            transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
-            if (rb.transform.position.x > 1)
-            {
-                rb.velocity = new Vector3(speed * -1, 0f, 0f);
-            }
-            else if (rb.transform.position.x < -1) rb.velocity = new Vector3(speed, 0f, 0f);
-        }
         if (transform.position.z <= -2.0f) Destroy(gameObject);
 	}
 
 	void OnTriggerStay(Collider other)
 	{
-        if (gameObject.tag != "Coin" && other.gameObject.CompareTag("crumpleZone"))
+        if (other.gameObject.CompareTag("crumpleZone"))
         {
             speed = other.transform.parent.gameObject.GetComponent<Rigidbody> ().velocity.magnitude;
 			rb.velocity = direction * speed;

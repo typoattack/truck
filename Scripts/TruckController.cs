433534a551560f63075a58c2d64e70c2e05d4f5f
typoattack <38441671+typoattack@users.noreply.github.com>
2019-10-01 19:45:04 -0400

    Revert "Revert "Optimization""

--- Scripts/TruckController.cs
+++ Scripts/TruckController.cs
@@ -1,39 +1,39 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TruckController : MonoBehaviour {
+public class TruckController : MonoBehaviour
+{
 
-    private Rigidbody rb;
     public float speed = 1.0f;
-	private Vector3 direction;
+    private Rigidbody rb;
+    private Vector3 direction;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
-		direction = transform.right;
+        direction = transform.right;
         rb.velocity = transform.right * speed;
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         if (transform.position.z <= -2.0f) Destroy(gameObject);
-	}
+    }
 
-	void OnTriggerStay(Collider other)
-	{
+    void OnTriggerStay(Collider other)
+    {
         if (other.gameObject.CompareTag("crumpleZone"))
         {
-            speed = other.transform.parent.gameObject.GetComponent<Rigidbody> ().velocity.magnitude;
-			rb.velocity = direction * speed;
-		} 
-	}
+            speed = other.transform.parent.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+            rb.velocity = direction * speed;
+        }
+    }
 
     IEnumerator StopTruckTemporarily(float duration)
     {
-        rb.velocity = transform.right * 0f;
+        rb.velocity = Vector3.zero;
         yield return new WaitForSeconds(duration);
         rb.velocity = transform.right * speed;
 

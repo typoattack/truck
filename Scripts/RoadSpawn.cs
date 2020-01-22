4df851acc486a1998ccf3266c7176a0c2596a549
typoattack <38441671+typoattack@users.noreply.github.com>
2020-01-22 16:09:19 -0500

    double coins
    replace double jump with double coins, add new textures for roads, add dedicated 4-lane and 6-lane road prefabs

--- Scripts/RoadSpawn.cs
+++ Scripts/RoadSpawn.cs
@@ -5,12 +5,15 @@ public class RoadSpawn : MonoBehaviour {
     public RoadController roadLR;
     public RoadController roadRL;
     public RoadController ground;
-    private int groundDecider;
+    public RoadController fourLaneRoad;
+    public RoadController sixLaneRoad;
+    public int groundDecider;
     private int rotationDecider;
-    private int roadCount = 10;
+    public int roadCount = 10;
     public int lastRoad = 20;
-    int spawnMode = 0; // 0 for random, 1 for 2+2 highways, 2 for 3+3 highways
-    int highwayCounter = 0;
+    public int spawnMode = 0; // 0 for random, 1 for 2+2 highways, 2 for 3+3 highways
+    public int highwayCounter = 0;
+    public int pauseCounter = 0;
 
     // Use this for initialization
     void Start ()
@@ -28,55 +31,78 @@ public class RoadSpawn : MonoBehaviour {
     
     void SpawnGround(int i)
     {
-        groundDecider = Random.Range(0, 10);
-        rotationDecider = Random.Range(-7, 7);
-
-        Vector3 pos = new Vector3(transform.position.x, transform.position.y, i);
-
-        if (groundDecider >= 9 && spawnMode == 0) spawnMode = 1;
-        else if (groundDecider >= 8 && spawnMode == 0) spawnMode = 2;
-        if (spawnMode == 1 && roadCount == 0)
+        if (pauseCounter > 0)
         {
-            if (highwayCounter < 2) Instantiate(roadLR, pos, transform.rotation);
-            else Instantiate(roadRL, pos, transform.rotation * Quaternion.Euler(0, 180f, 0));
-            
-            highwayCounter++;
-            if (highwayCounter == 4)
-            {
-                spawnMode = 0;
-                highwayCounter = 0;
-                roadCount = 4;
-            }
+            pauseCounter--;
         }
-        else if (spawnMode == 2 && roadCount == 0)
-        {
-            if (highwayCounter < 3) Instantiate(roadLR, pos, transform.rotation);
-            else Instantiate(roadRL, pos, transform.rotation * Quaternion.Euler(0, 180f, 0));
 
-            highwayCounter++;
-            if (highwayCounter == 6)
-            {
-                spawnMode = 0;
-                highwayCounter = 0;
-                roadCount = 6;
-            }
-        }
-        else if (groundDecider <= 2 || roadCount > 2)
-        {
-            Instantiate(ground, pos, transform.rotation);
-            roadCount = 0;
-        }
         else
         {
-            Quaternion newRotation = transform.rotation;
-            //if (rotationDecider <= 0) newRotation *= Quaternion.Euler(0, 180f, 0);
-            if (rotationDecider <= 0) Instantiate(roadLR, pos, transform.rotation);
+            groundDecider = Random.Range(0, 10);
+            rotationDecider = Random.Range(-7, 7);
+
+            Vector3 pos = new Vector3(transform.position.x, transform.position.y, i);
+
+            if (groundDecider >= 9 && spawnMode == 0) spawnMode = 1;
+            else if (groundDecider >= 8 && spawnMode == 0) spawnMode = 2;
+            if (spawnMode == 1 && roadCount == 0 && i >= 4)
+            {
+                /*
+                if (highwayCounter < 2) Instantiate(roadLR, pos, transform.rotation);
+                else Instantiate(roadRL, pos, transform.rotation * Quaternion.Euler(0, 180f, 0));
+
+                highwayCounter++;
+                if (highwayCounter == 4)
+                {
+                    spawnMode = 0;
+                    highwayCounter = 0;
+                    roadCount = 4;
+                }
+                */
+                Vector3 pos4 = new Vector3(transform.position.x, transform.position.y, i + 3);
+                Instantiate(fourLaneRoad, pos4, transform.rotation);
+                spawnMode = 0;
+                pauseCounter = 3;
+                roadCount = 4;
+            }
+            else if (spawnMode == 2 && roadCount == 0 && i >= 6)
+            {
+                /*
+                if (highwayCounter < 3) Instantiate(roadLR, pos, transform.rotation);
+                else Instantiate(roadRL, pos, transform.rotation * Quaternion.Euler(0, 180f, 0));
+
+                highwayCounter++;
+                if (highwayCounter == 6)
+                {
+                    spawnMode = 0;
+                    highwayCounter = 0;
+                    roadCount = 6;
+                }
+                */
+                Vector3 pos6 = new Vector3(transform.position.x, transform.position.y, i + 5);
+                Instantiate(sixLaneRoad, pos6, transform.rotation);
+                spawnMode = 0;
+                pauseCounter = 5;
+                roadCount = 6;
+            }
+            else if (groundDecider <= 2 || roadCount > 2)
+            {
+                Instantiate(ground, pos, transform.rotation);
+                roadCount = 0;
+            }
             else
             {
-                newRotation *= Quaternion.Euler(0, 180f, 0);
-                Instantiate(roadRL, pos, newRotation);
+                Quaternion newRotation = transform.rotation;
+                //if (rotationDecider <= 0) newRotation *= Quaternion.Euler(0, 180f, 0);
+                if (rotationDecider <= 0) Instantiate(roadLR, pos, transform.rotation);
+                else
+                {
+                    newRotation *= Quaternion.Euler(0, 180f, 0);
+                    Instantiate(roadRL, pos, newRotation);
+                }
+                roadCount++;
             }
-            roadCount++;
         }
+        
     }
 }
